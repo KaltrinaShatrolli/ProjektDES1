e23a0f275b81bec749c3d46e289c0b6e92afd83f
Lindita Lura <linditalura983@gmail.com>
2026-04-02 17:15:27 +0200

    Pjesa 3:"Enkriptimi dhe Dekriptimi i File" Lindita Lura

--- AlgoritmiDES.java
+++ AlgoritmiDES.java
@@ -21,6 +21,26 @@ public class AlgoritmiDES {
             System.out.println("Teksti Origjinal: " + plainteksti);
             System.out.println("Teksti i Enkriptuar: " + tekstiEnkriptuar);
             System.out.println("Teksti i Dekriptuar: " + tekstiDekriptuar);
+
+            // Enkriptimi i file
+            String emriFajllit = "plaintext.txt";
+            String permbajtja = "Ky eshte teksti qe ndodhet brenda file dhe do te enkriptohet me DES.";
+            Files.write(Paths.get(emriFajllit), permbajtja.getBytes());
+
+            
+            byte[] fajlliByte = Files.readAllBytes(Paths.get(emriFajllit));
+            byte[] fajlliEnkriptuarByte = operacioniCipher(fajlliByte, celesi, Cipher.ENCRYPT_MODE);
+            Files.write(Paths.get("fajlli_enkriptuar.des"), fajlliEnkriptuarByte);
+
+            // Dekriptimi i file
+            byte[] teDhenatEnkriptuara = Files.readAllBytes(Paths.get("fajlli_enkriptuar.des"));
+            byte[] fajlliDekriptuarByte = operacioniCipher(teDhenatEnkriptuara, celesi, Cipher.DECRYPT_MODE);
+            Files.write(Paths.get("fajlli_i_dekriptuar.txt"), fajlliDekriptuarByte);
+
+            System.out.println("\nFile");
+            System.out.println("File u enkriptua dhe u rikthye me sukses!");
+            System.out.println("Procesi Perfundoi");
+
         } catch (Exception e) 
         { e.printStackTrace(); 
 
@@ -43,6 +63,7 @@ public class AlgoritmiDES {
         return cipher.doFinal(data);
     }
 }
+
      
     
   
